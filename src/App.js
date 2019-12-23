9c8100a90eb39a434978e4f99ca9fc9112dc1577
Heeko <jaheeko@gmail.com>
2019-12-23 18:59:07 -0500

    added sound, colorname

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GiSpeaker } from "react-icons/gi";
+import { colors } from "./colors.json";
+
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = { color: "blue" };
+  }
+
+  color_changer() {
+    const num = Math.ceil(Math.random() * colors.length);
+    this.setState({ color: colors[num] });
+  }
+
+  speaker() {
+    const msg = new SpeechSynthesisUtterance(this.state.color);
+    window.speechSynthesis.speak(msg);
+  }
+
+  render() {
+    return (
+      <div className="button" onClick={() => this.color_changer()} style={{ backgroundColor: this.state.color }}>
+        {this.state.color}
+        <GiSpeaker onClick={(e) => { e.stopPropagation(); this.speaker(); }} stroke="black" strokeWidth="4" />
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Main />
     </div>
   );
 }
