298b51df96f119f2ac8c2b72cdbc6466fa81a2b3
Heeko <jaheeko@gmail.com>
2019-12-23 19:09:27 -0500

    Updates

---
+++ precache-manifest.47f341081242cde81c8327cb6471ff60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ebbba8406cbbaebcd6e6dbe56c90b1",
+    "url": "/color-clicker/index.html"
+  },
+  {
+    "revision": "f19319989f2142935927",
+    "url": "/color-clicker/static/css/main.e757ba59.chunk.css"
+  },
+  {
+    "revision": "bef3f1b0ac327508c51a",
+    "url": "/color-clicker/static/js/2.cee63927.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/color-clicker/static/js/2.cee63927.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f19319989f2142935927",
+    "url": "/color-clicker/static/js/main.78f117fc.chunk.js"
+  },
+  {
+    "revision": "a8460e25e0b54e684d31",
+    "url": "/color-clicker/static/js/runtime-main.deab89bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-clicker/static/media/logo.5d5d9eef.svg"
+  }
+]);
